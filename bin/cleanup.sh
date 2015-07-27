18becacde7f474b1a0512ca091d6bf5bf125ec26
gjvoit <gjvoit@yahoo.com>
2015-07-27 15:07:17 -0400

    Update cleanup.sh
    more cleanup changes

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,2 +1,4 @@
 for i in  `docker ps -q`; do docker stop $i; done
 docker rm `docker ps --no-trunc -aq`
+sudo docker ps -a -q --filter "status=exited" | xargs sudo docker rm
+sudo docker rmi `sudo docker images -q --filter "dangling=true"`
