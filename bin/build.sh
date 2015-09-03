7d5d7674e9b396cd6d5fcc2aea55e3817b28bc4b
gjvoit <gjvoit@yahoo.com>
2015-09-03 11:38:57 +0000

    There was a timing issue when running build.sh, I added a sleep before a docker run api - ching chen

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,8 @@ docker run --name frontend -p 80:9000 -d frontend
 docker build -t database ../api/database/postgres
 docker run --name pgdb -p 5432:5432 -d database
 docker build -t api ../api
+# There is a timing issue here, and the sleep is to account for it. The front end was not hooking up with database in time.
+sleep 2
 docker run --name api -p 3000:3000 --link pgdb:db -e "RAILS_ENV=docker" -d api
 docker run \
   --volume=/:/rootfs:ro \
